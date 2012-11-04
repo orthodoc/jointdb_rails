1b8f09b60f0a7eaa3e0dcf310661dbb3bcb37b82
Biswajit Dutta Baruah <bdbaruah@orthodoc.in>
2012-11-04 19:06:18 +0530

    added default companies and categories to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,19 @@ puts 'New user created: ' << user.name
 user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
 puts 'New user created: ' << user2.name
 user.add_role :admin
+
+puts 'SETTING UP DEFAULT CATEGORIES'
+category1 = Category.create(:name => 'Manufacturer1')
+puts 'New category created: ' << category1.name
+category2 = Category.create(:name => 'Manufacturer2')
+puts 'New category created: ' << category2.name
+
+puts 'SETTING UP DEFAULT COMPANIES'
+company1 = Company.create(:name => 'Smart Ortho1', :category_id => 1)
+puts 'New company created: ' << company1.name
+company2 = Company.create(:name => 'Smart Ortho2', :category_id => 1)
+puts 'New company created: ' << company2.name
+company3 = Company.create(:name => 'Smart Ortho3', :category_id => 2)
+puts 'New company created: ' << company3.name
+company4 = Company.create(:name => 'Smart Ortho4', :category_id => 2)
+puts 'New company created: ' << company4.name
