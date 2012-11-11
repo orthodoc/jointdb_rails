7140e94cf369dadda78eb3f767130f4585291103
Biswajit Dutta Baruah <bdbaruah@orthodoc.in>
2012-11-12 00:13:54 +0530

    corrected pagination malfunction due to routes confusion

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Jointdb::Application.routes.draw do
   authenticated :user do
     root :to => 'companies#index'
   end
-  root :to => "home#index"
+  root :to => "companies#index"
   devise_for :users
   resources :users, :only => [:show, :index]
   resources :companies
